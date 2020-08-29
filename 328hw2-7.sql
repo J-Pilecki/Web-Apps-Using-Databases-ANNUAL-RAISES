b5dfaf4d77d64edc407cd28d9eb8263542433754
jp431hsu <60199696+jp431hsu@users.noreply.github.com>
2020-08-28 17:38:13 -0700

    Create 328hw2-7.sql

---
+++ 328hw2-7.sql
@@ -0,0 +1,37 @@
+spool 328hw2-7-out.txt
+
+prompt Justin Pilecki
+
+/* This function adds $2 to an employee's salary as part of the first year's
+   raise when an employee passes review */
+
+create or replace function give_raise(emp int)
+   return integer as
+
+temp_salary integer;
+
+begin
+   select salary
+   into temp_salary
+   from Employee
+   where emp = empl_id;
+
+   temp_salary := temp_salary + 2;
+
+   return temp_salary;
+
+end;
+/
+show errors
+
+var emp number
+
+exec :emp := give_raise(1)
+
+print emp
+
+exec :emp := give_raise(9)
+
+print emp
+
+spool off
