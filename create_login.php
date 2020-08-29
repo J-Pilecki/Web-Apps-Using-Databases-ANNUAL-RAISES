30dc2c410fb3d552f0ed600833b79504b6922e8d
jp431hsu <60199696+jp431hsu@users.noreply.github.com>
2020-08-28 17:39:46 -0700

    Create create_login.php

---
+++ create_login.php
@@ -0,0 +1,45 @@
+<?php
+
+/*=====
+  START w/ a STUB!
+
+function create_login()
+{
+    ?>
+    <p> called create_login() </p>
+    <?php
+}
+=====*/
+
+/*=====
+  REPLACE with the "full" function
+=====*/
+
+/*=====
+    function: create_login: void -> void
+    purpose: expects nothing, returns nothing, and has the side-effect
+        of outputting to the resulting document an Oracle log-on form
+        with method="post" and action equal to the calling PHP document
+
+    requires: name-pwd-fieldset.html
+
+    last modified: 2020-04-13
+=====*/
+
+function create_login()
+{
+    // create the desired Oracle log-in form
+    ?>
+      	<form method="post"
+              action="<?= htmlentities($_SERVER['PHP_SELF'],
+                                       ENT_QUOTES) ?>">
+        <?php
+	require_once("name-pwd-fieldset.html");
+        ?>
+            <input type="submit" value="login" />
+        </form>
+        <?php
+}
+
+?>
+
